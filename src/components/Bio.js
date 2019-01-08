80d3b83fee509a4ab466cff88a9da817c9b88f9c
ryosua <hi@ryanyosua.me>
2019-01-07 20:31:53 -0500

    Init gatsby blog.

---
+++ src/components/Bio.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
+
+import { rhythm } from '../utils/typography'
+
+function Bio() {
+  return (
+    <StaticQuery
+      query={bioQuery}
+      render={data => {
+        const { author, social } = data.site.siteMetadata
+        return (
+          <div
+            style={{
+              display: `flex`,
+              marginBottom: rhythm(2.5),
+            }}
+          >
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              style={{
+                marginRight: rhythm(1 / 2),
+                marginBottom: 0,
+                minWidth: 50,
+                borderRadius: `100%`,
+              }}
+            />
+            <p>
+              I am a full-time software engineer and part-time entrepreneur. I
+              currently work at Energage as a software engineer. In my free
+              time, I am working on Fragmented, a decentralized social network
+              for Ethereum enthusiasts.
+            </p>
+          </div>
+        )
+      }}
+    />
+  )
+}
+
+const bioQuery = graphql`
+  query BioQuery {
+    avatar: file(absolutePath: { regex: "/profile_pic.png/" }) {
+      childImageSharp {
+        fixed(width: 50, height: 50) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+      }
+    }
+  }
+`
+
+export default Bio
