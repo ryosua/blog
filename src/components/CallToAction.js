c26617db0981c47706cf8356bf52a438569fa832
ryosua <hi@ryanyosua.me>
2020-08-08 14:29:28 -0600

    removed project

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -8,11 +8,7 @@ const ItalizedText = styled.p`
 const CallToAction = () => (
   <ItalizedText>
     If you enjoyed this post, please subsribe to my{' '}
-    <a href="http://eepurl.com/c8xBc9">email newsletter</a> and{' '}
-    <a href="https://www.youtube.com/channel/UClqbavbIJWxl-iMBpHb37JA">
-      Youtube channel
-    </a>
-    .
+    <a href="http://eepurl.com/c8xBc9">email newsletter</a>.
   </ItalizedText>
 )
 
